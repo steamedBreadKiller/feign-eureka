5cb3b322ca94beae72f5a4f6cbfd72bbf6e6dc89
wangjingbiao <jingbiao.wang@aihuishou.com>
2021-03-25 19:23:58 +0800

    改造官方feign使用示例，用于测试feign支持的配置项

--- server/src/main/java/demo/HelloServerApplication.java
+++ server/src/main/java/demo/HelloServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,22 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Spencer Gibb
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 @RestController
 public class HelloServerApplication {
-	@Autowired
-	DiscoveryClient client;
 
-	@RequestMapping("/")
-	public String hello() {
-		List<ServiceInstance> instances = client.getInstances("HelloServer");
-		ServiceInstance selectedInstance = instances
-				.get(new Random().nextInt(instances.size()));
-		return "Hello World: " + selectedInstance.getServiceId() + ":" + selectedInstance
-				.getHost() + ":" + selectedInstance.getPort();
-	}
+    @RequestMapping("/{name}")
+    public String hello(@PathVariable(value = "name") String name) {
+        return String.format("hello feignClient:%s, I am feignServer", name);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServerApplication.class, args);
+    }
 }
