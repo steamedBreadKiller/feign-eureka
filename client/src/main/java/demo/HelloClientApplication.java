10292ef251e0c38dc82c1493f163e54236e67e4e
wangjingbiao <jingbiao.wang@aihuishou.com>
2021-03-29 18:23:41 +0800

    增加熔断配置

--- client/src/main/java/demo/HelloClientApplication.java
+++ client/src/main/java/demo/HelloClientApplication.java
@@ -1,10 +1,12 @@
 package demo;
 
+import feign.hystrix.FallbackFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,9 +34,38 @@ public class HelloClientApplication {
         SpringApplication.run(HelloClientApplication.class, args);
     }
 
-    @FeignClient(name = "opt-quotation-order-service", url = "http://127.0.0.1:7111")
+    @FeignClient(name = "serviceName", url = "http://127.0.0.1:7111",
+            fallbackFactory = HelloClientApplication.HelloClientFallBackFactory.class
+    )
     interface HelloClient {
         @RequestMapping("/{name}")
         String hello(@PathVariable(value = "name") String name);
     }
+
+    /**
+     * feign调用错误时的回调
+     */
+    @Component
+    static class HelloClientFallBack implements HelloClient {
+
+        @Override
+        public String hello(String name) {
+            return "you get an error callback";
+        }
+    }
+
+    @Component
+    static class HelloClientFallBackFactory implements FallbackFactory<HelloClient> {
+
+        @Override
+        public HelloClient create(Throwable throwable) {
+            return new HelloClient() {
+                @Override
+                public String hello(String name) {
+                    return "you get an error, is: " + throwable.getMessage();
+                }
+            };
+        }
+    }
+
 }
